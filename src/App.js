422bf6ff8e3cbca52ca1122d35e1f6bb92117b0d
Julian Leong <julianljk@Julians-MacBook-Pro.local>
2017-12-16 20:18:11 -0600

    formatting

--- src/App.js
+++ src/App.js
@@ -5,63 +5,63 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			counter: 0,
-			sliderValue: 0,
-			showSlider: true
-		}
-	}
-	componentWillMount() {
-		setInterval(() => {
-			this.setState({
-				counter: this.state.counter + this.state.sliderValue
-			});
-		}, 10000)
-	}
-	onSliderChange = (e) => {
-		this.setState({
-			sliderValue: Number(e.target.value)
-		});
-	}
-	onToggle = (e) => {
-		this.setState({
-			showSlider: !this.state.showSlider
-		});
-	}
-	render() {
-		let {
+    constructor() {
+        super();
+        this.state = {
+            counter: 0,
+            sliderValue: 0,
+            showSlider: true
+        }
+    }
+    componentWillMount() {
+        setInterval(() => {
+            this.setState({
+                counter: this.state.counter + this.state.sliderValue
+            });
+        }, 10000)
+    }
+    onSliderChange = (e) => {
+        this.setState({
+            sliderValue: Number(e.target.value)
+        });
+    }
+    onToggle = (e) => {
+        this.setState({
+            showSlider: !this.state.showSlider
+        });
+    }
+    render() {
+        let {
 			counter,
-			sliderValue,
-			showSlider
+            sliderValue,
+            showSlider
 		} = this.state;
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<button type="button" onClick={this.onToggle}>Toggle Slider</button>
-				{
-					showSlider &&
-					<div>
-						<Slider
-							counter={counter}
-							onSliderChange={this.onSliderChange}
-							sliderValue={sliderValue}
-						/>
-						<Printer
-							value={counter}
-							negativeToken={"[̲̅$̲̅(̲̅-1)̲̅$̲̅]"}
-							positiveToken={"[̲̅$̲̅(̲̅1)̲̅$̲̅]"}
-						/>
-					</div>
-				}
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <button type="button" onClick={this.onToggle}>Toggle Slider</button>
+                {
+                    showSlider &&
+                    <div>
+                        <Slider
+                            counter={counter}
+                            onSliderChange={this.onSliderChange}
+                            sliderValue={sliderValue}
+                        />
+                        <Printer
+                            value={counter}
+                            negativeToken={"[̲̅$̲̅(̲̅-1)̲̅$̲̅]"}
+                            positiveToken={"[̲̅$̲̅(̲̅1)̲̅$̲̅]"}
+                        />
+                    </div>
+                }
 
-			</div>
-		);
-	}
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 
 class Slider extends Component {
-  componentWillMount() {
-    console.log("mouting");
-  }
-  componentWillUnmount() {
-    console.log("unmounting");
-  }
-  render() {
-    return (
-      <div>
-        <p>
-          Counter value: {this.props.counter}
-        </p>
-        <div>
-          {-10} <input type="range" min={-10} max={10} step={1} onChange={this.props.onSliderChange} value={this.props.sliderValue} /> {10}
-        </div>
-        <div>
-          {this.props.sliderValue}
-        </div>
-      </div>
-    );
-  }
+    componentWillMount() {
+        console.log("mouting");
+    }
+    componentWillUnmount() {
+        console.log("unmounting");
+    }
+    render() {
+        return (
+            <div>
+                <p>
+                    Counter value: {this.props.counter}
+                </p>
+                <div>
+                    {-10} <input type="range" min={-10} max={10} step={1} onChange={this.props.onSliderChange} value={this.props.sliderValue} /> {10}
+                </div>
+                <div>
+                    {this.props.sliderValue}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Slider;
