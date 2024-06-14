147d704238614f9d601dde9a77d56afd51088dd6
Julian Leong <julianljk@Julians-MacBook-Pro.local>
2024-06-14 11:02:57 +0800

    update packages

--- src/actions/counterActions.js
+++ src/actions/counterActions.js
@@ -1,16 +1,15 @@
-export const SET_SLIDER_VALUE = Symbol('SET_SLIDER_VALUE');
-export const ADD_TO_COUNTER = Symbol('ADD_TO_COUNTER');
+export const SET_SLIDER_VALUE = "SET_SLIDER_VALUE";
+export const ADD_TO_COUNTER = "ADD_TO_COUNTER";
 
 export function setSliderValue(value) {
-    return {
-        type: SET_SLIDER_VALUE,
-        value
-    }
+  return {
+    type: SET_SLIDER_VALUE,
+    value,
+  };
 }
 
 export function addToCounter() {
-    return {
-        type: ADD_TO_COUNTER
-    }
+  return {
+    type: ADD_TO_COUNTER,
+  };
 }
-
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducer from './reducer/index.js';
-let store = createStore(reducer); 
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducer/index.js";
+let store = createStore(reducer);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-, document.getElementById('root'));
-registerServiceWorker();
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
